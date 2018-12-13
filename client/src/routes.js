32ace120cc67f876e85915a93b3f771c3ceeae34
Chizzy <chizoba.princess@gmail.com>
2018-12-13 13:58:16 -0500

    setting up front end auth

---
+++ client/src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Route} from "react-router-dom";
+import HomePage from './containers/HomePage'
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+
+const BaseRouter = () => (
+    <div>
+        <Route exact path="/" component={HomePage}/>{" "}
+        <Route exact path="/login/" component={Login}/>{" "}
+        <Route exact path="/signup/" component={Signup}/>{" "}
+    </div>
+);
+
+export default BaseRouter;
--- main/models.py
+++ main/models.py
@@ -32,7 +32,7 @@ class Cart(models.Model):
 
 class User(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField(max_lentgh=255)
+    email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
