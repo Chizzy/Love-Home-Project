b2561b114552694dd6e62d54c583debee2794c35
CB_ATL_113_Smile_Rodnez <cb_atl_113_smile_rodnez@MacBook-Air-9.local>
2018-12-17 12:05:07 -0500

    took old image out

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -10,14 +10,26 @@ class HomePage extends Component {
                 {/* These are the images for the homepage. Some of them are hyperlinks */}
             {/* Dont need this image <img width="100%" src="https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1574&q=80" alt="#"/>     */}
             <div className="splashdiv">
-            <img id="splash" width="1230" src="https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt=""/>
-            <div className="wordsdiv">  Home design that fits your style, not someone else's.
+            <img id="splash" src="https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt=""/>
+            
+                <div className="wordsdiv">  Home design that fits your style, not someone else's.
 
             </div>
             </div>
 
+            <center><h1 id="how">How It Works</h1></center>
+            <div className="bigdiv">
+                <div className="hiw">
+                    <img src="http://i64.tinypic.com/359d4qh.png" alt=""/><p id="steps"> 1. Pick Your Design Package</p>
+                </div>
+                <div className="hiw">
+                    <img src="http://i67.tinypic.com/spf1ux.png" alt=""/><p id="steps"> 2. Shop From Our Recomended Products</p>
+                </div>
+                <div className="hiw">
+                    <img src="http://i68.tinypic.com/fw2lwz.png" alt=""/><p id="steps"> 3. We Ship Your Selections Directly To You!</p>
+                </div>
+            </div>
 
-            <img width="100%" src="http://i65.tinypic.com/jue15u.png" alt=""/>
             <h1>Check out our Instagram inspiration feed!</h1>
             
 
