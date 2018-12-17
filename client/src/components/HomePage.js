55c0a1987d6f206233002ce5c55d50fdc9ce4184
CB_ATL_113_Smile_Rodnez <cb_atl_113_smile_rodnez@MacBook-Air-9.local>
2018-12-17 14:54:55 -0500

    fixed landing image

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,64 +3,85 @@ import styled from 'styled-components'
 import SimpleSlider from './SimpleSlider'
 import './HomePage.css'
 
+const SplashImage = styled.div`
+    background: url('https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+
+    .wordsdiv {
+        margin: auto;
+        height: 40%;
+        width: 60%;
+        border: white solid;
+        background-color: whitesmoke;
+        position: relative;
+        opacity: 0.8;
+        font-size: 50px;
+        color: black;
+        display: flex;
+
+    div {
+        margin: auto;
+        padding: 10px;
+        text-align: center;
+    }
+    }
+`
+
 class HomePage extends Component {
     render() {
         return (
             <div>
                 {/* These are the images for the homepage. Some of them are hyperlinks */}
-            {/* Dont need this image <img width="100%" src="https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1574&q=80" alt="#"/>     */}
-            <div className="splashdiv">
-            <img id="splash" src="https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt=""/>
-            
-                <div className="wordsdiv">  Home design that fits your style, not someone else's.
-
-            </div>
-            </div>
-
-            <center><h1 id="how">How It Works</h1></center>
-            
-            
-            <div className="bigdiv">
-                <div className="hiw">
-                
-                    <img src="http://i64.tinypic.com/359d4qh.png" alt=""/>
-                   
-                    
-                        <p id="steps"> 1. Pick Your Design Package</p>
-                       
+                {/* Dont need this image <img width="100%" src="https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1574&q=80" alt="#"/>     */}
+                <div className="splashdiv">
+                    {/* <img id="splash" width="100%" src="https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt=""/> */}
+                    <SplashImage>
+                        <div className="wordsdiv"><div>Home design that fits your style, not someone else's.</div></div>
+                    </SplashImage>
                 </div>
-                <div className="hiw">
-              
-                    <img src="http://i67.tinypic.com/spf1ux.png" alt=""/>
-                    
-                    <p id="steps"> 2. Shop From Our Recomended Products</p>
-                    
+
+                <center><h1 id="how">How It Works</h1></center>
+
+
+                <div className="bigdiv">
+                    <div className="hiw">
+                        <img src="http://i64.tinypic.com/359d4qh.png" alt="" />
+                        <center>
+                            <p className="stepfont" id="steps1"> 1. Pick Your Design Package</p>
+                        </center>
+                    </div>
+                    <div className="hiw">
+                        <img id="middlepic" src="http://i67.tinypic.com/spf1ux.png" alt="" />
+                        <center>
+                            <p className="stepfont" id="steps2"> 2. Shop From Our Recommended Products</p>
+                        </center>
+                    </div>
+                    <div className="hiw">
+                        <img src="http://i68.tinypic.com/fw2lwz.png" alt="" />
+                        <center>
+                            <p className="stepfont" id="steps3"> 3. We Ship Your Selections Directly To You!</p>
+                        </center>
+                    </div>
                 </div>
-                <div className="hiw">
-                    
-                        <img src="http://i68.tinypic.com/fw2lwz.png" alt=""/>
-                        
-                    <p id="steps"> 3. We Ship Your Selections Directly To You!</p> 
-                    
-                    
-                </div>
+
+                <h1>Check out our Instagram inspiration feed!</h1>
+
+
+                {/* This is the code for the photocarousel for "instagram" */}
+
+
+                <SimpleSlider />
+
+                {/* <PureReactCarousel/> */}
+
+
+
             </div>
 
-            <h1>Check out our Instagram inspiration feed!</h1>
-            
-
-{/* This is the code for the photocarousel for "instagram" */}
-
-
-<SimpleSlider />
-
-{/* <PureReactCarousel/> */}
-
-
-
-            </div>
-            
-            );
+        );
     }
 }
 
