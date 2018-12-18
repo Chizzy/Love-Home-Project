56c6503452e9d69640748cbc16329264d959152e
Chizzy <chizoba.princess@gmail.com>
2018-12-18 10:13:17 -0500

    auth

--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -4,8 +4,13 @@ import './LogInPage.css'
 
 class LogInPage extends Component {
     state = {
+        name: '',
         email: '',
-        password: ''
+        password: '',
+        address: '',
+        city: '',
+        zip_code: '',
+        phone_number: ''
     }
 
     handleChange = (event) => {
@@ -27,26 +32,26 @@ class LogInPage extends Component {
                 <div className="log-in" id="log-in-button">
                     <p className="log-in-header">Log In</p>
                 </div>
-                <form id="sign-up-form">
+                <form onSubmit={event => this.props.handleSignUp(event, this.state)} id="sign-up-form">
                     <div className="header">
                         <h1>Sign Up for Free</h1>
                     </div>
-                    <input className="first-name" type="text" placeholder="First Name*" required/>
-                    <input className="last-name" type="text" placeholder="Last Name*" required/>
-                    <input type="email" placeholder="Email Address*" required/>
-                    <input type="password" placeholder="Set a password*" required/>
-                    <button id="sign-up-submit">Get Started</button>
+                    <input className="name" type="text" name="name" placeholder="Name*" value={this.state.name} onChange={this.handleChange} required/>
+                    <input type="email" name="email" placeholder="Email Address*" value={this.state.email} onChange={this.handleChange} required/>
+                    <input type="password" name="password" placeholder="Set a password*" value={this.state.password} onChange={this.handleChange} required/>
+                     <input type="text" name="address" placeholder="Address*" value={this.state.address} onChange={this.handleChange} required/>
+                     <input type="text" name="city" placeholder="City*" value={this.state.city} onChange={this.handleChange} required/>
+                     <input type="text" name="zip_code" placeholder="Zip Code*" value={this.state.zip_code} onChange={this.handleChange} required/>
+                     <input type="text" name="phone_number" placeholder="Phone Number*" value={this.state.phone_number} onChange={this.handleChange} required/>
+                    <button type="submit" id="sign-up-submit">Get Started</button>
                 </form>
-                <form id="log-in-form">
+                <form onSubmit={event => this.props.handleLogIn(event, this.state)} id="log-in-form">
                     <div className="header">
                         <h1>Welcome back!</h1>
                     </div>
-                    <input className="email" type="email" placeholder="Email Address*" required/>
-                    <input type="password" placeholder="Your password*" required/>
-                    <div className="password">
-                        Forgot Password?
-                    </div>
-                    <button id="log-in-submit">Log in</button>
+                    <input className="email" name="email" type="email" placeholder="Email Address*" value={this.state.email} onChange={this.handleChange} required/>
+                    <input type="password" name="password" placeholder="Your password*" value={this.state.password} onChange={this.handleChange} required/>
+                    <button type="submit" id="log-in-submit">Log In</button>
                 </form>
 
             </div>
@@ -55,3 +60,8 @@ class LogInPage extends Component {
 }
 
 export default LogInPage;
+
+LogInPage.propTypes = {
+  handleSignUp: PropTypes.func.isRequired,
+    handleLogIn: PropTypes.func.isRequired
+};
